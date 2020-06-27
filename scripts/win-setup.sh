fa25da6999be758420d19b54b438ec3f61eccf79
zmird <z@mird.com>
2020-06-27 16:08:02 +0200

    Updated windows setup script

--- scripts/win-setup.sh
+++ scripts/win-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Run ansible setup
-vagrant winrm --shell powershell --command "./setup/Upgrade-Powershell.ps1"
-vagrant winrm --shell powershell --command "./setup/ConfigureRemotingForAnsible.ps1"
-vagrant winrm --shell powershell --command "./setup/Install-WMF3Hotfix.ps1"
+vagrant winrm --shell powershell --command "./Upgrade-Powershell.ps1"
+vagrant winrm --shell powershell --command "./ConfigureRemotingForAnsible.ps1"
+vagrant winrm --shell powershell --command "./Install-WMF3Hotfix.ps1"
 
 # Setup icmp v4
-vagrant winrm --shell powershell --command 'netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow'
+vagrant winrm --shell powershell --command "./Enable-ICMP-v4.ps1"
